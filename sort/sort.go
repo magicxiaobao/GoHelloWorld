8e5e2d7cab201cb9f398fce62632d9f7ce5973d9
chenyuejun <chenyuejun@lightinthebox.com>
2021-11-03 23:48:00 +0800

    sort built-in & custom type implement Interface

---
+++ sort/sort.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	intsSlice := []int{1, 2, 6, 3}
+	sort.Ints(intsSlice)
+	fmt.Println(intsSlice)
+	stringsSlice := []string{"a", "C", "g", "e"}
+	sort.Strings(stringsSlice)
+	fmt.Println(stringsSlice)
+	stringSorted := sort.StringsAreSorted(stringsSlice)
+	fmt.Println(stringSorted)
+
+	students := []student{{"guoguo", 9}, {"suisui", 37}}
+	sort.Sort(studentByAge(students))
+	fmt.Println(students)
+}
+
+type student struct {
+	name string
+	age  int
+}
+
+type studentByAge []student
+
+func (s studentByAge) Len() int {
+	return len(s)
+}
+
+func (s studentByAge) Less(i, j int) bool {
+	return s[i].age > s[j].age
+}
+
+func (s studentByAge) Swap(i, j int) {
+	var t = s[i]
+	s[i] = s[j]
+	s[j] = t
+}
